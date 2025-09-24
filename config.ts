df31b077705c70e0ad8f48ecb1f8e59ad181f1d2
BrendanA1622 <89553639+BrendanA1622@users.noreply.github.com>
2025-09-23 20:17:39 -0700

    Reverted / to config.ts

--- config.ts
+++ config.ts
@@ -4,5 +4,5 @@ const isProd = process.env.NODE_ENV === 'production';
 
 export const repoName = 'sweeps-bench';
 
-export const basePath = isProd ? `/${repoName}/` : '';
+export const basePath = isProd ? `/${repoName}` : '';
 export const assetPrefix = isProd ? `/${repoName}/` : '';
